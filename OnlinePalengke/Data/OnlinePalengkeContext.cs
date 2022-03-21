678e677c8ab444a102f0dcdfb95f233dc4527c5d
TECH4ED-PC5 <TECH4ED-PC5@TECH4ED_5>
2022-03-21 14:52:43 +0800

    Add project files.

---
+++ OnlinePalengke/Data/OnlinePalengkeContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OnlinePalengke.sheet;
+
+namespace OnlinePalengke.Data
+{
+    public class OnlinePalengkeContext : DbContext
+    {
+        public OnlinePalengkeContext (DbContextOptions<OnlinePalengkeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<OnlinePalengke.sheet.Class> Class { get; set; }
+    }
+}
---
+++ OnlinePalengke/Pages/main2/Details.cshtml.cs
@@ -0,0 +1,41 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OnlinePalengke.Data;
+using OnlinePalengke.sheet;
+
+namespace OnlinePalengke.Pages.main2
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly OnlinePalengke.Data.OnlinePalengkeContext _context;
+
+        public DetailsModel(OnlinePalengke.Data.OnlinePalengkeContext context)
+        {
+            _context = context;
+        }
+
+        public Class Class { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Class = await _context.Class.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Class == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
